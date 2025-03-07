48581fe96da5638f6212b9457bf8ce225c69a617
Sipxng <athiy2ahsyifa@gmail.com>
2025-03-07 11:33:28 +0700

    initial codes

---
+++ laravel-registrasi/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+
+Route::post('/submit', [RegisterController::class, 'submit']);
+
+Route::view('/register', 'register');
+Route::get('/', function () {
+    return view('welcome');
+});
